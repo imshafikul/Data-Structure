f7077a5d5e4ef68b3ee023395ebb5df357913c21
Shafikul Islam <sumonaits@gmail.com>
2018-03-31 12:44:16 +0600

    Insertion sort in ascending and descending order

---
+++ Algorithm/sorting/Insertionsort.php
@@ -0,0 +1,29 @@
+<?php
+
+  /*
+  Insertion sort in Ascending Order
+  */ 
+
+
+  function insertionsort(array $array): array
+  {
+    $lenght = count($array);
+    for($i=0; $i<$lenght; $i++){
+      $j = $i;
+      while($j>0 && $array[$j] < $array[$j - 1]){
+        $temp = $array[$j];
+        $array[$j] = $array[$j -1];
+        $array[$j -1] = $temp;
+        $j--;
+      }
+    }
+
+    return $array;
+
+  }
+  
+  $sorted_array = insertionsort([5,8,1,6]);
+
+  echo implode(",", $sorted_array);
+
+?>
