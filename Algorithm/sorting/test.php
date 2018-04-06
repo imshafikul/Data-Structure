cbd54883f552c63ecf9a17c835c2f217f09dc24b
Shafikul Islam <sumonaits@gmail.com>
2018-04-06 23:28:17 +0600

    BFS for Graph

--- Algorithm/sorting/test.php
+++ Algorithm/sorting/test.php
@@ -1,4 +1,28 @@
 <?php
 
-//
+function merge($left, $right){
+	$res = array();
+	while (count($left) > 0 && count($right) > 0){
+		if($left[0] > $right[0]){
+			$res[] = $right[0];
+			$right = array_slice($right , 1);
+		}else{
+			$res[] = $left[0];
+			$left = array_slice($left, 1);
+		}
+	}
+	while (count($left) > 0){
+		$res[] = $left[0];
+		$left = array_slice($left, 1);
+	}
+	while (count($right) > 0){
+		$res[] = $right[0];
+		$right = array_slice($right, 1);
+  }
+
+	print_r( $res);
+}
+
+
+ merge([12,4,5], [8,1]);
 ?>
